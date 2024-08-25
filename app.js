ec8905f502e2b7e0ca1c41d0aca50a14f33f7c2d
sebastian-gab <g.sebastian27@outlook.com>
2024-08-25 16:02:56 +0300

    Create Navigation Menu

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let wrapperMenu = document.querySelector('.wrapper-menu');
-
-wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');  
-})
+document.getElementById("menu-button").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevents the default anchor behavior
+  document.getElementById("header__button").classList.toggle("hamburger--open");
+  document.querySelector(".menu-holder").classList.toggle("show");
+});
